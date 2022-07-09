e6f5b828152b222d5fca23df6fe3236ce0f43738
ProkhorchukCursor <danyloprokhorchuk@gmail.com>
2022-07-09 10:30:08 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// // HTML-elements
+
+// import {
+//  getIdButtonEl,
+//  getIdResultEl,
+//  fontGeneratorDecrementButtonEl,
+//  fontGeneratorResultButtonEl,
+//  fontGeneratorIncrementButtonEl,
+//  fontGeneratorResultEl,
+//  bodyEl,
+// } from "./elements.js";
+
+// // Functions
+
+// import { idGenerator, fontGenerator } from "./functions.js";
+
+// // EventListeners
+
+// // 1)
+
+// getIdButtonEl.addEventListener("click", () => {
+//  getIdResultEl.textContent = idGenerator.next().value;
+// });
+
+// // 2)
+
+// fontGeneratorDecrementButtonEl.addEventListener("click", () => {
+//  const fontSize = fontGenerator.next("down").value;
+//  bodyEl.style.fontSize = `${fontSize}px`;
+//  fontGeneratorResultEl.textContent = `${fontSize}px`;
+// });
+
+// fontGeneratorResultButtonEl.addEventListener("click", () => {
+//  const fontSize = fontGenerator.next().value;
+//  fontGeneratorResultEl.textContent = `${fontSize}px`;
+// });
+
+// fontGeneratorIncrementButtonEl.addEventListener("click", () => {
+//  const fontSize = fontGenerator.next("up").value;
+//  bodyEl.style.fontSize = `${fontSize}px`;
+//  fontGeneratorResultEl.textContent = `${fontSize}px`;
+// });
+console.log("Hello");
